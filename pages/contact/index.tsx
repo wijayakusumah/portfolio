6d6b4f9a13d13efc5627232e75176805950fc366
devnicetry <devnicetry@gmail.com>
2024-11-29 15:24:34 +0700

    add contact
    pages and components

---
+++ pages/contact/index.tsx
@@ -0,0 +1,13 @@
+import { Header } from '@/components/header';
+import { ContactInfo } from '@/components/contact';
+import { Footer } from '@/components/footer';
+
+export default function HomePage() {
+    return (
+        <>
+            <Header />
+            <ContactInfo />
+            <Footer />
+        </>
+    );
+}
